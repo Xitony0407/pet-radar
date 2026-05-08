bad8812931220621fbbbe33047bcc3bc38e9ac8b
Ximena Gutierrez Perez <gutierrezperezximena4@gmail.com>
2026-05-08 17:05:53 -0600

    fix: update Application Insights configuration

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
+import * as appInsights from 'applicationinsights';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+
+appInsights.setup('TU_CONNECTION_STRING')
+.setAutoDependencyCorrelation(true)
+.setAutoCollectRequests(true)
+.setAutoCollectPerformance(true, true)
+.setAutoCollectExceptions(true)
+.start();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
